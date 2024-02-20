00f9fbbd6cb7bf88bbb82fb9f38c2200fb5f94de
Divit Mittal <64.69.76.69.74.m@gmail.com>
2024-02-20 18:57:54 +0530

    Updated Spicetify

--- Mackup/.config/fish/functions/ya.fish
+++ Mackup/.config/fish/functions/ya.fish
@@ -2,7 +2,7 @@ function ya
 	set tmp (mktemp -t "yazi-cwd.XXXXX")
 	yazi $argv --cwd-file="$tmp"
 	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		cd -- "$cwd"
+		builtin cd -- "$cwd"
 	end
 	rm -f -- "$tmp"
 end
