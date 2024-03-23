bec5c0f5abd7b7ef3cf47f078db5a42bd5baebc6
Divit Mittal <64.69.76.69.74.m@gmail.com>
2024-03-24 04:16:59 +0530

    updates to fish fzf

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,17 +8,17 @@ set -g fish_greeting ''
 set -l brew_prefix '/usr/local'
 
 if status --is-interactive
-    # Emulates vim's cursor shape behavior
-    set -g fish_vi_force_cursor 1
-    # Set the normal and visual mode cursors to a block
-    set -g fish_cursor_default block
-    # Set the insert mode cursor to a line
-    set -g fish_cursor_insert line
-    # Set the replace mode cursor to an underscore
-    set -g fish_cursor_replace_one underscore
-    # The following variable can be used to configure cursor shape in
-    # visual mode, but due to fish_cursor_default, is redundant here
-    set -g fish_cursor_visual block
+  # Emulates vim's cursor shape behavior
+  set -g fish_vi_force_cursor 1
+  # Set the normal and visual mode cursors to a block
+  set -g fish_cursor_default block
+  # Set the insert mode cursor to a line
+  set -g fish_cursor_insert line
+  # Set the replace mode cursor to an underscore
+  set -g fish_cursor_replace_one underscore
+  # The following variable can be used to configure cursor shape in
+  # visual mode, but due to fish_cursor_default, is redundant here
+  set -g fish_cursor_visual block
 end
 
 ## Environment Variables
@@ -71,99 +71,107 @@ fish_add_path $brew_prefix/opt/postgresql@14/bin
 fish_add_path $HOME/.cargo/bin
 
 if status --is-interactive
-    ####################################   Config when fish is interactive ###############################################3
-    fish_add_path $HOME/.config/emacs/bin # Doom Emacs
-    # fish_add_path /System/Library/PrivateFrameworks/Apple80211.framework/Resources #Airport Utility
+  ####################################   Config when fish is interactive ###############################################3
+  fish_add_path $HOME/.config/emacs/bin # Doom Emacs
+  # fish_add_path /System/Library/PrivateFrameworks/Apple80211.framework/Resources #Airport Utility
 
-    ## Ruby
-    # rbenv
-    rbenv init - fish | source
+  ## Ruby
+  # rbenv
+  rbenv init - fish | source
 
-    ## Python
-    # pip
-    alias pip-uninstall-all 'pip freeze | cut -d "@" -f1 | xargs pip uninstall -y'
-    # pyenv
-    set -gx PYENV_ROOT $HOME/.pyenv
-    fish_add_path $PYENV_ROOT/bin
-    pyenv init - | source
-    pyenv virtualenv-init - | source
+  ## Python
+  # pip
+  alias pip-uninstall-all 'pip freeze | cut -d "@" -f1 | xargs pip uninstall -y'
+  # pyenv
+  set -gx PYENV_ROOT $HOME/.pyenv
+  fish_add_path $PYENV_ROOT/bin
+  pyenv init - | source
+  pyenv virtualenv-init - | source
+
+  ## FZF - fuzzy finder
+  # fzf
+  set -gx FZF_DEFAULT_OPTS "--multi --cycle --border --height 50% --bind='space:toggle' --bind='tab:down' --bind='btab:up' --no-scrollbar --marker='*' --preview-window=wrap"
+  set -gx FZF_DEFAULT_COMMAND 'fd --hidden'
+
+  # PatrickF1/fzf.fish plugin environment variables
+  set -gx fzf_fd_opts --hidden
+  set -gx fzf_preview_dir_cmd eza --all --color=always --icons=always --classify --group-directories-first --group --hyperlink --color-scale --color-scale-mode=gradient
+  set fzf_diff_highlighter delta --paging=never --width=20
+  set fzf_preview_file_cmd bat
+  fzf_configure_bindings --directory=\ef --variables=\ev --processes=\ep --git_status=\es --git_log=\el --history=
+
+  # fifc plugin environment variables
+  set -gx fifc_editor nvim
+  set -gx fifc_fd_opts --hidden
+  set -gx fifc_bat_opts --style=numbers
+  set -gx fifc_exa_opts --all --classify --icons --oneline --group-directories-first --group
+
+  # GNU Screen config env var
+  set -gx SCREENRC $HOME/.config/screen/screenrc
+
+  ############################################ Aliases #################################################
+  alias showpath 'echo $PATH | sed "s/ /\n/g"'
+  alias showid "id | sed 's/ /\n/g' | sed 's/,/\n/g'"
+
+  # Mapping "ls" to "eza"
+  set -l eza_params "--all" "--classify" "--icons=always" "--group-directories-first" "--color=always" "--color-scale" "--color-scale-mode=gradient" "--hyperlink"
+  alias ll "eza -lbhHigUmuSa@ $eza_params"
+  alias lt "eza -T --level=2 $eza_params" # tree listing with depth 2
+  alias ls "eza $eza_params"
+
+  # Other similar mappings
+  alias man 'batman'
+  alias cat 'bat'
+  alias ff 'fastfetch --logo-type iterm --logo $HOME/Sync-macOS/assets/a-12.png --pipe false --structure Title:OS:Kernel:Uptime:Display:Terminal:CPU:CPUUsage:GPU:Memory:Swap:LocalIP --gpu-temp true --cpu-temp true --title-color-user magenta --title-color-at blue --cpu-format "{1} @ {#4;35}{8}{#}" --gpu-format "{2} @ {#4;35}{4}{#}"'
+
+  # Directory shortcuts for macOS
+  alias dt "cd $HOME/Desktop/"
+  alias dl "cd $HOME/Downloads/"
+
+  # Ultimate aliases
+  alias apps-backup "env ls /Applications/ 1> $HOME/Sync-macOS/etc/ref-txts/apps_(date +%b%y).txt"
+  alias gem-ultimate 'sudo gem update; sudo gem cleanup'
+  alias pipx-ultimate 'pipx upgrade-all; pipx list --short 1> ~/Sync-macOS/etc/ref-txts/pipx_list.txt'
+  alias brew-ultimate 'brew update; and brew upgrade; and brew autoremove; and brew cleanup -s --prune=0; and brew bundle dump --force; and rm -rf (brew --cache)'
+  alias mac-ultimate 'sudo -v; brew-ultimate; apps-backup'
+
+  # Recursively delete `.DS_Store` files for macOS
+  alias cleanup-DS "sudo find . -type f -name '*.DS_Store' -ls -delete"
+
+  # Empty the Trash on all mounted volumes and the main drive & clear system logs for macOS
+  alias empty-trash 'bass "sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /private/tmp/*.log"'
+
+  # Abbreviations
+  abbr --position anywhere --add nv nvim
+  abbr --position anywhere --add v vim
+  abbr --position anywhere --add .2 'cd ../..'
+  abbr --position anywhere --add .3 'cd ../../..'
+  abbr --position command --add gits 'git status'
+  abbr --position command --add gitph 'git push'
+  abbr --position command --add gitpl 'git pull'
+  abbr --position command --add gitf 'git fetch'
+  abbr --position command --add gitc 'git commit'
 
 
-    # fifc plugin environment variables
-    set -gx fifc_editor nvim
-    set -gx fifc_fd_opts --hidden
-    set -gx fifc_bat_opts --style=numbers
-    set -gx fifc_exa_opts --all --classify --icons --oneline --group-directories-first --group
+  ####################################### Initializations ###############################################
+  # Run Fastfetch - fetch system info
+  if type -q fastfetch && test "$TERM_PROGRAM" = "WezTerm" && test "$TERM" = "xterm-256color"
+    ff
+  end
 
-    # PatrickF1/fzf.fish plugin environment variables
-    set -gx fzf_fd_opts --hidden
-    set -gx fzf_preview_dir_cmd eza --all --color=always --icons=always --classify --group-directories-first --group --hyperlink --color-scale --color-scale-mode=gradient
-    # GNU Screen config env var
-    set -gx SCREENRC $HOME/.config/screen/screenrc
+  # Starship - custom shell prompt
+  if type -q starship
+    starship init fish | source
+  end
 
-    ############################################ Aliases #################################################
-    alias showpath 'echo $PATH | sed "s/ /\n/g"'
-    alias showid "id | sed 's/ /\n/g' | sed 's/,/\n/g'"
+  # Zoxide utility - smarter cd
+  if type -q zoxide
+    zoxide init --cmd cd fish | source
+  end
 
-    # Mapping "ls" to "eza"
-    set -l eza_params "--all" "--classify" "--icons=always" "--group-directories-first" "--color=always" "--color-scale" "--color-scale-mode=gradient" "--hyperlink"
-    alias ll "eza -lbhHigUmuSa@ $eza_params"
-    alias lt "eza -T --level=2 $eza_params" # tree listing with depth 2
-    alias ls "eza $eza_params"
-
-    # Other similar mappings
-    alias man 'batman'
-    alias cat 'bat'
-    alias ff 'fastfetch --logo-type iterm --logo $HOME/Sync-macOS/assets/a-12.png --pipe false --structure Title:OS:Kernel:Uptime:Display:Terminal:CPU:CPUUsage:GPU:Memory:Swap:LocalIP --gpu-temp true --cpu-temp true --title-color-user magenta --title-color-at blue --cpu-format "{1} @ {#4;35}{8}{#}" --gpu-format "{2} @ {#4;35}{4}{#}"'
-
-    # Directory shortcuts for macOS
-    alias dt "cd $HOME/Desktop/"
-    alias dl "cd $HOME/Downloads/"
-
-    # Ultimate aliases
-    alias apps-backup "env ls /Applications/ 1> $HOME/Sync-macOS/etc/ref-txts/apps_(date +%b%y).txt"
-    alias gem-ultimate 'sudo gem update; sudo gem cleanup'
-    alias pipx-ultimate 'pipx upgrade-all; pipx list --short 1> ~/Sync-macOS/etc/ref-txts/pipx_list.txt'
-    alias brew-ultimate 'brew update; and brew upgrade; and brew autoremove; and brew cleanup -s --prune=0; and brew bundle dump --force; and rm -rf (brew --cache)'
-    alias mac-ultimate 'sudo -v; brew-ultimate; apps-backup'
-
-    # Recursively delete `.DS_Store` files for macOS
-    alias cleanup-DS "sudo find . -type f -name '*.DS_Store' -ls -delete"
-
-    # Empty the Trash on all mounted volumes and the main drive & clear system logs for macOS
-    alias empty-trash 'bass "sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sudo rm -rfv /private/tmp/*.log"'
-
-    # Abbreviations
-    abbr --position anywhere --add nv nvim
-    abbr --position anywhere --add v vim
-    abbr --position anywhere --add .2 'cd ../..'
-    abbr --position anywhere --add .3 'cd ../../..'
-    abbr --position command --add gits 'git status'
-    abbr --position command --add gitph 'git push'
-    abbr --position command --add gitpl 'git pull'
-    abbr --position command --add gitf 'git fetch'
-    abbr --position command --add gitc 'git commit'
-
-
-    ####################################### Initializations ###############################################
-    # Run Fastfetch - fetch system info
-    if type -q fastfetch && test "$TERM_PROGRAM" = "WezTerm" && test "$TERM" = "xterm-256color"
-        ff
-    end
-
-    # Starship - custom shell prompt
-    if type -q starship
-        starship init fish | source
-    end
-
-    # Zoxide utility - smarter cd
-    if type -q zoxide
-        zoxide init --cmd cd fish | source
-    end
-
-    # Atuin - magical shell history
-    if type -q atuin
-        atuin init fish | source
-    end
+  # Atuin - magical shell history
+  if type -q atuin
+    atuin init fish | source
+  end
 end
 
